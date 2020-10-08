50f0de480bff98c35caa753bd50bf2bdd6b65ba4
h0stage <63636560+h0stage@users.noreply.github.com>
2020-10-08 12:29:31 +0200

    Imported .gitignore template
    https://gist.github.com/brazilnut2000/8226958

---
+++ Race_Simulator_C_Sharp_(VS)/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Race_Simulator_C_Sharp__VS_
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
